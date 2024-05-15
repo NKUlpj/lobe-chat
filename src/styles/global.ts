897ff0677493f9bb7ca9b7cae4c8890a02b5bc8e
NKUlpj <33405631+NKUlpj@users.noreply.github.com>
2024-05-15 16:49:01 +0800

    Update global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,12 +5,14 @@ import { Theme, css } from 'antd-style';
 // ref: https://zhuanlan.zhihu.com/p/113855026
 export default ({ token }: { prefixCls: string; token: Theme }) => css`
   @import url("https://cdn.jsdelivr.net/npm/cn-fontsource-lxgw-wen-kai-gb-screen@1.0.6/font.min.css");
+
+  html, body, #__next, *{
+      font-family:"LXGW WenKai GB Screen","HarmonyOS Sans","Segoe UI","SF Pro Display",-apple-system,BlinkMacSystemFont,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif,"HarmonyOS Sans SC","PingFang SC","Hiragino Sans GB","Microsoft Yahei UI","Microsoft Yahei","Source Han Sans CN",sans-serif,"Segoe UI Emoji","Segoe UI Symbol","Apple Color Emoji","Twemoji Mozilla","Noto Color Emoji","Android Emoji" !important;
+  };
   html,
   body,
   #__next {
     position: relative;
-    font-family:"LXGW WenKai GB Screen","HarmonyOS Sans","Segoe UI","SF Pro Display",-apple-system,BlinkMacSystemFont,Roboto,Oxygen,Ubuntu,Cantarell,"Open Sans","Helvetica Neue",sans-serif,"HarmonyOS Sans SC","PingFang SC","Hiragino Sans GB","Microsoft Yahei UI","Microsoft Yahei","Source Han Sans CN",sans-serif,"Segoe UI Emoji","Segoe UI Symbol","Apple Color Emoji","Twemoji Mozilla","Noto Color Emoji","Android Emoji" !important;
-
     overscroll-behavior: none;
 
     height: 100%;
